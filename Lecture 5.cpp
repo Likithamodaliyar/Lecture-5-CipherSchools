e45c822fa53188d7c807a9f098e74ec3b5aaaa5f
Likitha Modaliyar <119063547+Likithamodaliyar@users.noreply.github.com>
2024-07-12 20:10:00 +0530

    Add files via upload

---
+++ Lecture 5.cpp
@@ -0,0 +1,30 @@
+//Execute AND and OR statements and print it out 
+
+#include<iostream>
+using namespace std;
+int main(){
+//	int a,b;
+	
+//	cout<<"Input 2 integers a and b"<<endl;
+//	cin>>a;
+//	cin>>b;
+//	cout<<a,b;
+
+// Next is Declare the 4 variables and this code for the and ,or operations.
+
+  int a,b;
+  int c,d;
+  
+  cin>>a;
+  cin>>b;
+  cin>>c;
+  cin>>d;
+  
+  cout<<(a>b&&c>d);
+  cout<<(a>b ||c>d);
+  
+  return 0;
+}
+
+
+
